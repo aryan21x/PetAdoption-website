10043bb50c2d951a4a2968df380f2dd268f09b6f
Miller, Augustus S. <augustus.miller@mines.sdsmt.edu>
2024-04-23 19:58:48 -0600

    added tables and mock values

--- SqlScript.sql
+++ SqlScript.sql
@@ -2,13 +2,57 @@ create database if not exists project;
 
 use project;
 
+drop table if exists user_table;
+drop table if exists pets;
+drop table if exists workers;
+drop table if exists shelters;
+drop table if exists adopters;
+drop table if exists associatedBusinesses;
+drop table if exists vets;
+
+
 create table if not exists user_table
 			(user_id int primary key auto_increment not null, username varchar(255) not null, 
             password varchar(255) not null);
             
--- get some users
-INSERT INTO user_table (username, password)
-VALUES 
-('Joe', 'Rogan'),
-('Gandhi', 'SoHungry'),
-('Elizabeth', 'EmpressOfIndia');
+create table if not exists shelters
+			(shelter_id int primary key auto_increment not null, address varchar(255) not null, 
+            pets int, phoneNumber double, name varchar(255) not null);
+            
+create table if not exists workers
+			(worker_id int primary key auto_increment not null, fName varchar(255) not null, 
+            lName varchar(255) not null,  email varchar(255), address varchar(255) not null, 
+            shelter_id int,
+			foreign key(shelter_id) references shelters(shelter_id));     
+            
+create table if not exists adopters
+			(adopt_id int primary key auto_increment not null, fName varchar(255) not null, 
+            lName varchar(255) not null,  email varchar(255), address varchar(255) not null, 
+            pets int, phoneNumber double);            
+            
+create table if not exists associatedBusinesses
+			(business_id int primary key auto_increment not null, address varchar(255) not null, 
+            service varchar(255), phoneNumber double,  email varchar(255),  name varchar(255) not null);       
+            
+create table if not exists vets
+			(vet_id int primary key auto_increment not null, fName varchar(255) not null, 
+            lName varchar(255) not null,  email varchar(255), address varchar(255) not null, 
+            phoneNumber double,
+			businessName varchar(255));
+            
+create table if not exists pets
+			(pet_id int primary key auto_increment not null, name varchar(255) not null, 
+            breed varchar(255) not null, shelter_id int not null, foreign key(shelter_id) references shelters(shelter_id), 
+            species varchar(255) not null, age int, adoptered bool, 
+            worker_id int not null, vet_id int, adopt_id int,
+            foreign key(worker_id) references workers(worker_id), foreign key(vet_id) references vets(vet_id),
+            foreign key(adopt_id) references adopters(adopt_id));
+            
+
+            
+
+            
+
+            
+
+            
